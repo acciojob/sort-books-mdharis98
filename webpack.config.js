71181c33f639bddb1f9b15761f4a55fe558515ae
Haris <mohammadharis7871@gmail.com>
2025-01-06 13:28:31 +0530

    commited

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         path: path.join(__dirname,"/dist"),
         filename: "index_bundle.js",
     },
+    watchOptions: {
+        ignored: /DumpStack\.log\.tmp|node_modules/,
+      },
     module:{
         rules: [
             {
